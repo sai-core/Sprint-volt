7b19d2c6aad51f7f3c0f74fbed4af0fb4b2295a1
𝑺𝒶𝒾 ⚝ <sfaiz7307@gmail.com>
2026-06-20 20:57:02 +0530

    Change card click behavior to navigate to watch page

--- app.js
+++ app.js
@@ -89,13 +89,11 @@ function render(){
   moviesEl.innerHTML = filtered.map((movie, i) => cardTemplate(movie, i)).join("");
 
   moviesEl.querySelectorAll(".card").forEach(card => {
-  card.addEventListener("click", () => {
-    const link = card.dataset.link;
-    if (link) {
-      window.open(link, "_blank");
-    }
+    card.addEventListener("click", () => {
+      location.href = `watch.html?id=${card.dataset.id}`;
+    });
   });
-});
+}
 
 function cardTemplate(movie, index){
   const title = escapeHtml(movie.title || "Untitled");
