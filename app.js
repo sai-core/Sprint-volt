ede907408303c8343e66c4357245dc0cbb08b6cd
𝑺𝒶𝒾 ⚝ <sfaiz7307@gmail.com>
2026-06-20 16:53:41 +0530

    Update Supabase URL in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://kvosgbxpqnuimaievngy.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://kvosgbxpqnuimaievngy.supabase.co";
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2b3NnYnhwcW51aW1haWV2bmd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NDAxMDUsImV4cCI6MjA5NzUxNjEwNX0.4xyHmpouSbWvZSD5z0mDhvf175S_dubHTBbMvojJRqE";
 
